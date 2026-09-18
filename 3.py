30fb965a8161dd834eb71bc5325997286630098c
Mehrab Azan <77406365+MehrabAzan@users.noreply.github.com>
2026-09-18 05:35:49 -0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 def quick_sort(A, lo, hi):
     if lo < hi:
         pivot = partition(A, lo, hi)
-        return quick_sort(A, lo, pivot) + quick_sort(A, pivot + 1, hi)
+        return quick_sort(A, lo, pivot) + [pivot] + quick_sort(A, pivot + 1, hi)
 
 def partition(A, lo, hi):
     pivot = A[lo]
@@ -16,4 +16,4 @@ def partition(A, lo, hi):
 
     return leftwall
 
-print(quick_sort([3, 6, 8, 10, 1, 2, 1], 0, 6))
+print(quick_sort([3, 6, 8, 10, 1, 2, 1], 0, 7))
